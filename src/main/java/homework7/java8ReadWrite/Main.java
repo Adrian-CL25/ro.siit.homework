b4f327474b8461bef96b75418f81616ea6ecad54
Adrian-CL25 <adimex09@gmail.com>
2021-03-06 12:26:58 +0200

    Homework 7

--- src/main/java/homework7/java8ReadWrite/Main.java
+++ src/main/java/homework7/java8ReadWrite/Main.java
@@ -1,9 +1,8 @@
 package homework7.java8ReadWrite;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /*By using streams and lamba expressions, implement a small application which:
@@ -16,16 +15,20 @@ Application takes 3 params:
 2. target month (1-12)
 3. output file name*/
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        String fileLocation = "src\\main\\resources\\in.txt";
-        String citire;
+        String location = "src\\main\\resources\\in.txt";
+//        Path p = Paths.get(location);
+
+        String input;
+        int targetMounth = 10;
 
         List<Persoana> personList = new ArrayList<>();
         try {
-            BufferedReader br = new BufferedReader(new FileReader(fileLocation));
-            while ((citire = br.readLine()) != null) {
-                Persoana person = Persoana.mapLineToPersoana(citire);
+            BufferedReader br = new BufferedReader(new FileReader(location));
+            while ((location = br.readLine()) != null && location.length() > 0) {
+                Persoana person = Persoana.mapLineToPersoana(location);
                 personList.add(person);
 
             }
@@ -33,9 +36,37 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Persons list: ");
+        System.out.println(personList.toString());
 
+
+        System.out.println("Ordered alphabetically  birthday on a month equals 10.\n");
+
+        String str = personList.stream()
+                .filter(persoana -> persoana.getDateOfBirhday().getMonth().getValue() == targetMounth)
+                .sorted(Comparator.comparing(persoana -> persoana.getFirstName()))
+                .map(p -> p.getFirstName() + "," + p.getLastName() + " ")
+                .reduce("", (n1, n2) -> n1 + n2);
+        System.out.println(str);
+
+        File destination = new File("src/main/resources/out.txt");
+
+        try (BufferedWriter scrie = new BufferedWriter(new FileWriter(destination))) {
+
+            scrie.write(str +"\n");
+            scrie.newLine();
+                scrie.newLine();
+
+
+//                writer.close();
+
+        }
 
     }
+
+
 }
+
+
 
 
--- src/main/java/homework7/java8ReadWrite/Persoana.java
+++ src/main/java/homework7/java8ReadWrite/Persoana.java
@@ -1,9 +1,8 @@
 package homework7.java8ReadWrite;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Builder
 @Data
@@ -13,7 +12,7 @@ import lombok.Setter;
 public class Persoana {
     private String firstName;
     private String lastName;
-    private String dateOfBirhday;
+    private LocalDate dateOfBirhday;
 
 
     public static Persoana mapLineToPersoana(String line) {
@@ -23,12 +22,17 @@ public class Persoana {
         return Persoana.builder()
                 .firstName(fields[0])
                 .lastName(fields[1])
-                .dateOfBirhday(fields[2])
+                .dateOfBirhday(mapResultToLocalDate(fields[2]))
                 .build();
     }
 
-//    public static LocalDate mapResultToLocalDate(String date) {
-//        String[] time = date.split("-");
-//        return LocalDate.of(Integer.parseInt(time[0]),Integer.parseInt(time[1]), Integer.parseInt(time[2]));
-//    }
+    public static LocalDate mapResultToLocalDate(String date) {
+        String[] time = date.split("-");
+        return LocalDate.of(Integer.parseInt(time[0]),Integer.parseInt(time[1]), Integer.parseInt(time[2]));
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName +" "+ dateOfBirhday +"\n";
+    }
 }
